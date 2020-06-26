a25f7e192ddb1ea1155eb58126e22422d5f28c1d
Usuario9342 <usuario934280@protonmail.com>
2020-06-26 19:05:35 -0300

    termino el efecto parallax para el fondo.

---
+++ main.js
@@ -0,0 +1,14 @@
+/* para lograr que el primer elemento tenga el desplazamiento paralax hay que darle un identificador y seleccionarlo*/ 
+
+const paralax = document.getElementById("paralax");
+
+window.addEventListener("scroll", function(){
+    let offset = window.pageYOffset
+    //console.log("offset" + offset);
+    //este de arriba es el offset normal del scroll de la pantalla.
+    //console.log()"offset + 0.7" + offset * 0.7
+    //con esta linea se altera el numero que nos da el scroll de la pantalla, ralentizandolo.
+
+    paralax.style.backgroundPositionY = offset * 0.7 + "px";
+
+})
