8dd8a15457202292840e2f24b0f6f3e201dcdf9d
Usuario9342 <usuario934280@protonmail.com>
2020-06-27 13:31:58 -0300

    le agrego una navigacion

--- main.js
+++ main.js
@@ -2,13 +2,16 @@
 
 const paralax = document.getElementById("paralax");
 
+
 window.addEventListener("scroll", function(){
     let offset = window.pageYOffset
+    
     //console.log("offset" + offset);
     //este de arriba es el offset normal del scroll de la pantalla.
     //console.log()"offset + 0.7" + offset * 0.7
     //con esta linea se altera el numero que nos da el scroll de la pantalla, ralentizandolo.
 
     paralax.style.backgroundPositionY = offset * 0.7 + "px";
+    
 
 })
