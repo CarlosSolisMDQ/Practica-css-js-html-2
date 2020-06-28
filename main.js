fab1932e74943493061e836203e2db1d04d29d5d
Usuario9342 <usuario934280@protonmail.com>
2020-06-27 22:26:41 -0300

    todo por hoy

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const paralax = document.getElementById("paralax");
 window.addEventListener("scroll", function(){
     let offset = window.pageYOffset
     
-    //console.log("offset" + offset);
+    // console.log("offset" + offset);
     //este de arriba es el offset normal del scroll de la pantalla.
     //console.log()"offset + 0.7" + offset * 0.7
     //con esta linea se altera el numero que nos da el scroll de la pantalla, ralentizandolo.
